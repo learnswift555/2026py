274994d6a24b4032c2a23568ee915b5d3a8fe3d3
learnswift555 <74773414+learnswift555@users.noreply.github.com>
2026-02-24 14:13:27 +0800

    Delete Day7 directory

--- Day7/module1.py
+++
@@ -1,6 +0,0 @@
-# 作者：柚子皮
-# 2026年02月20日14时57分47秒
-# yexixi2333@gmail.com
-
-def test1():
-    print("test1")
--- Day7/wd_message/__init__.py
+++
@@ -1,6 +0,0 @@
-# 作者：柚子皮
-# 2026年02月20日17时45分26秒
-# yexixi2333@gmail.com
-
-from . import send_message
-from . import receive_message
